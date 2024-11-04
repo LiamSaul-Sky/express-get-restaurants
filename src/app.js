8aeea08b03d94bb9f1d6e539b461bef364cf7f4c
LiamSaul-Sky <liam.saul@sky.uk>
2024-11-04 11:28:02 +0000

    Added functionality

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
-
+app.get("/restaurants", async(req, res)=>{
+    const allRestaurants = await Restaurant.findAll();
+    res.send(allRestaurants);
+});
 
 
 module.exports = app;
